2e64565925210cef8f771d24342c978479532e78
Dario <otrantodario@gmail.com>
2023-11-09 20:49:04 -0300

    Add Google Play Services

---
+++ Assets/Scripts/Menu/GooglePlayManager.cs
@@ -0,0 +1,33 @@
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+using UnityEngine;
+
+public class GooglePlayManager : MonoBehaviour
+{
+    public void Start()
+    {
+        SingIn();
+    }
+
+    public void SingIn()
+    {
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+            Debug.Log($"{nameof(GooglePlayManager)} : Sing in Success");
+        } 
+        else
+        {
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+            Debug.Log($"{nameof(GooglePlayManager)} : Sing in Failed");
+        }
+    }
+}
