0519446a5f150a482b9ff029907a7f26e1a36176
Dario <otrantodario@gmail.com>
2023-11-19 03:37:20 -0300

    Add Shop Icons

--- Assets/Scripts/Menu/MenuAnimationController.cs
+++ Assets/Scripts/Menu/MenuAnimationController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MenuAnimationController : MonoBehaviour
 {
     [SerializeField] private GameObject menuCanvas;
+    [SerializeField] private GameObject shopCanvas;
     [SerializeField] private Animator cameraAnimator;
     [SerializeField] private Animator gateAnimator;
     [SerializeField] private string startGameAnimationParam;
@@ -45,11 +46,13 @@ public class MenuAnimationController : MonoBehaviour
     {
         menuCanvas.SetActive(false);
         cameraAnimator.SetBool(startShopAnimationParam,true);
+        shopCanvas.SetActive(true);
     }
 
     public void ResetCameraAnimation()
     {
         menuCanvas.SetActive(true);
+        shopCanvas.SetActive(false);
         cameraAnimator.SetBool(startGameAnimationParam,false);
         cameraAnimator.SetBool(startShopAnimationParam,false);
     }
