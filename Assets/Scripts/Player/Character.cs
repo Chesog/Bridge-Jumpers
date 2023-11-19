4b4aa07a3e594a64ef9468c4f72ccb5527dbe7cd
Dario <otrantodario@gmail.com>
2023-11-19 04:23:42 -0300

    Add Shop Logic

---
+++ Assets/Scripts/Player/Character.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public GameObject character;
+    public float index;
+    public String name;
+    public float price;
+    public bool isBought;
+    public bool isSelected;
+}
---
+++ Assets/Scripts/Player/CharacterShop.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterShop : MonoBehaviour
+{
+    [SerializeField] private Character[] _characters;
+    [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private TextMeshProUGUI characterPrice;
+    private int _currentIndex;
+
+    private void OnEnable()
+    {
+        _currentIndex = 0;
+        int lenght = _characters.Length;
+        for (int i = 0; i < lenght; i++)
+        {
+            if (i == _currentIndex)
+                _characters[i].character.SetActive(true);
+            else
+                _characters[i].character.SetActive(false);
+        }
+        characterName.text = _characters[_currentIndex].name;
+        characterPrice.text = "$ : " + _characters[_currentIndex].price;
+    }
+
+    public void nextCharacter()
+    {
+        int lenght = _characters.Length;
+        _currentIndex++;
+        if (_currentIndex >= lenght)
+            _currentIndex = 0;
+
+        for (int i = 0; i < lenght; i++)
+        {
+            if (i == _currentIndex)
+                _characters[i].character.SetActive(true);
+            else
+                _characters[i].character.SetActive(false);
+        }
+
+        characterName.text = _characters[_currentIndex].name;
+        characterPrice.text = "$ : " + _characters[_currentIndex].price;
+    }
+
+    public void previousCharacter()
+    {
+        int lenght = _characters.Length;
+        _currentIndex--;
+        if (_currentIndex < 0)
+            _currentIndex = lenght - 1;
+
+        for (int i = 0; i < lenght; i++)
+        {
+            if (i == _currentIndex)
+                _characters[i].character.SetActive(true);
+            else
+                _characters[i].character.SetActive(false);
+        }
+    }
+}
