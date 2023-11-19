635311625a544a6fd338e69723a6da5b78ec9aba
Dario <otrantodario@gmail.com>
2023-11-19 18:37:28 -0300

    Fix Options Panel

--- Assets/Scripts/Menu/ButtonsController.cs
+++ Assets/Scripts/Menu/ButtonsController.cs
@@ -1,17 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class ButtonsController : MonoBehaviour
 {
+    [SerializeField] private GameObject MenuPanel;
     [SerializeField] private GameObject OptionsPanel;
     public void ToggleOptions()
     {
         if (OptionsPanel.activeInHierarchy)
+        {
             OptionsPanel.SetActive(false);
+            MenuPanel.SetActive(true);
+        }
         else
+        {
             OptionsPanel.SetActive(true);
+            MenuPanel.SetActive(false);
+        }
     }
 
     public void QuitGame()
