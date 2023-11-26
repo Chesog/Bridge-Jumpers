bab33e1af10bd024d46f55abdd31e3ebdc289ced
Dario <otrantodario@gmail.com>
2023-11-26 20:37:31 -0300

    Changed Game Achivements

--- Assets/Scripts/GooglePlay/PlayGamesArchivements.cs
+++ Assets/Scripts/GooglePlay/PlayGamesArchivements.cs
@@ -9,6 +9,12 @@ public class PlayGamesArchivements : MonoBehaviour
 {
     public static PlayGamesArchivements Instance;
     private bool hasBeenAutenticated;
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
     private void OnEnable()
     {
         if (Instance == null)
@@ -53,8 +59,29 @@ public class PlayGamesArchivements : MonoBehaviour
 #if UNITY_ANDROID
         if (hasBeenAutenticated)
         {
-            Social.ReportProgress("CgkI3Ji-p9sLEAIQAQ",100f, succes => { });
-            PlayerPrefs.SetString("FirstTime","FirstTime");
+            Social.ReportProgress("CgkI3Ji-p9sLEAIQAQ",100f, succes =>  {
+                if (succes)
+                {
+                    Debug.Log("Achievement unlocked: FirstTime");
+                    PlayerPrefs.SetString("FirstTime","FirstTime");
+                }
+                else
+                {
+                    Debug.LogError("Failed to unlock achievement: ");
+                }
+            });
+            
+            PlayGamesPlatform.Instance.ReportProgress("CgkI3Ji-p9sLEAIQAQ",100f, succes =>  {
+                if (succes)
+                {
+                    Debug.Log("Achievement unlocked: FirstTime");
+                    PlayerPrefs.SetString("FirstTime","FirstTime");
+                }
+                else
+                {
+                    Debug.LogError("Failed to unlock achievement: FirstTime");
+                }
+            });
         }
 #endif
     }
@@ -66,9 +93,30 @@ public class PlayGamesArchivements : MonoBehaviour
         {
             if (!PlayerPrefs.HasKey("TheMadKing"))
             {
-                Social.ReportProgress("CgkI3Ji-p9sLEAIQAg",100f, succes => { });
-                PlayerPrefs.SetString("TheMadKing","TheMadKing");
+                Social.ReportProgress("CgkI3Ji-p9sLEAIQAg",100f, succes => {
+                    if (succes)
+                    {
+                        Debug.Log("Achievement unlocked: TheMadKing");
+                        PlayerPrefs.SetString("TheMadKing","TheMadKing");
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to unlock achievement: ");
+                    }
+                });
             }
+            
+            PlayGamesPlatform.Instance.ReportProgress("CgkI3Ji-p9sLEAIQAg",100f, succes =>  {
+                if (succes)
+                {
+                    Debug.Log("Achievement unlocked: TheMadKing");
+                    PlayerPrefs.SetString("TheMadKing","TheMadKing");
+                }
+                else
+                {
+                    Debug.LogError("Failed to unlock achievement: TheMadKing");
+                }
+            });
         }
 #endif
     }
@@ -80,9 +128,30 @@ public class PlayGamesArchivements : MonoBehaviour
         {
             if (!PlayerPrefs.HasKey("LetTheMagicBegin"))
             {
-                Social.ReportProgress("CgkI3Ji-p9sLEAIQAw",100f, succes => { });
-                PlayerPrefs.SetString("LetTheMagicBegin","LetTheMagicBegin");
+                Social.ReportProgress("CgkI3Ji-p9sLEAIQAw",100f, succes => {
+                    if (succes)
+                    {
+                        Debug.Log("Achievement unlocked: LetTheMagicBegin");
+                        PlayerPrefs.SetString("LetTheMagicBegin","LetTheMagicBegin");
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to unlock achievement: LetTheMagicBegin");
+                    }
+                });
             }
+            
+            PlayGamesPlatform.Instance.ReportProgress("CgkI3Ji-p9sLEAIQAw",100f, succes =>  {
+                if (succes)
+                {
+                    Debug.Log("Achievement unlocked: LetTheMagicBegin");
+                    PlayerPrefs.SetString("LetTheMagicBegin","LetTheMagicBegin");
+                }
+                else
+                {
+                    Debug.LogError("Failed to unlock achievement: LetTheMagicBegin");
+                }
+            });
         }
 #endif
     }
@@ -94,8 +163,29 @@ public class PlayGamesArchivements : MonoBehaviour
         {
             if (!PlayerPrefs.HasKey("UniteTheKingdom"))
             {
-                Social.ReportProgress("CgkI3Ji-p9sLEAIQBA",100f, succes => { });
-                PlayerPrefs.SetString("UniteTheKingdom","UniteTheKingdom");
+                Social.ReportProgress("CgkI3Ji-p9sLEAIQBA",100f, succes => {
+                    if (succes)
+                    {
+                        Debug.Log("Achievement unlocked: UniteTheKingdom");
+                        PlayerPrefs.SetString("UniteTheKingdom","UniteTheKingdom");
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to unlock achievement: UniteTheKingdom");
+                    }
+                });
+                
+                PlayGamesPlatform.Instance.ReportProgress("CgkI3Ji-p9sLEAIQBA",100f, succes =>  {
+                    if (succes)
+                    {
+                        Debug.Log("Achievement unlocked: UniteTheKingdom");
+                        PlayerPrefs.SetString("UniteTheKingdom","UniteTheKingdom");
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to unlock achievement: UniteTheKingdom");
+                    }
+                });
             }
         }
 #endif
@@ -108,9 +198,30 @@ public class PlayGamesArchivements : MonoBehaviour
         {
             if (!PlayerPrefs.HasKey("AxeToMeetYou"))
             {
-                Social.ReportProgress("CgkI3Ji-p9sLEAIQBQ",100f, succes => { });
-                PlayerPrefs.SetString("AxeToMeetYou","AxeToMeetYou");
+                Social.ReportProgress("CgkI3Ji-p9sLEAIQBQ",100f, succes => {
+                    if (succes)
+                    {
+                        Debug.Log("Achievement unlocked: AxeToMeetYou");
+                        PlayerPrefs.SetString("AxeToMeetYou","AxeToMeetYou");
+                    }
+                    else
+                    {
+                        Debug.LogError("Failed to unlock achievement: ");
+                    }
+                });
             }
+            
+            PlayGamesPlatform.Instance.ReportProgress("CgkI3Ji-p9sLEAIQBQ",100f, succes =>  {
+                if (succes)
+                {
+                    Debug.Log("Achievement unlocked: AxeToMeetYou");
+                    PlayerPrefs.SetString("AxeToMeetYou","AxeToMeetYou");
+                }
+                else
+                {
+                    Debug.LogError("Failed to unlock achievement: AxeToMeetYou");
+                }
+            });
         }
 #endif
     }
